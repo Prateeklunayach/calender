23e7e800b47613f4b86376bcdd5c26ea420d53c0
Prateek Lunayach <145366020+Prateeklunayach@users.noreply.github.com>
2025-06-24 03:01:20 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,8 +15,8 @@ const Layout = ({ children }) => {
     <div className="flex min-h-screen relative">
       {/* Left Content Area */}
       <div className="2xl:w-1/2 w-[60%] min-h-screen relative">
-        <header className="fixed top-0 left-0 2xl:w-1/2 w-[60%] flex justify-between items-center px-8 py-6 bg-white z-100">
-          <div>
+{/*         <header className="fixed top-0 left-0 2xl:w-1/2 w-[60%] flex justify-between items-center px-8 py-6 bg-white z-100"> */}
+{/*           <div>
             <img style={{zIndex:"999"}} src="logo.webp" alt="Eye Logo" className="w-20 md:w-24 lg:w-28 ml-2 pt-2" />
           </div>
           <button className="hover:opacity-70 transition-opacity text-sm tracking-wide pt-1">
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
             </b>
              </button>
 
-        </header>
+        </header> */}
 
         <main className="pt-24 px-10 pl-2 w-full">
           {React.cloneElement(children)}
